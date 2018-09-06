76be7af32424530b747b1f049fd1f7d4ea0f54bc
Bradley Adams <bradley.wayne.adams@gmail.com>
2018-09-06 16:37:38 +1200

    second question draft

--- code.js
+++ code.js
@@ -9,8 +9,28 @@ function list(names){
       (i < (names.length - 2) ? ', ' : 
         (i === (names.length - 2) ? ' & ' : ''));
   }
-  console.log(answer)
   return answer
 }
 
-// Two:
+// Two: Valid Braces
+function validBraces(braces){
+
+  let comparison = []
+  let matching
+  let open = ['(', '{', '[']
+  let closed = [')', '}', ']']
+  
+  for (let i=0; i < braces.length; i++){
+    let position = braces[i]
+    
+    if (closed.indexOf(position) > -1){
+      matching = open[closed.indexOf(position)]
+      if (comparison.lengeth == 0 || (comparison.pop() != matching)){
+      return false
+      }
+    } else {
+    comparison.push(position)}
+    
+    }
+    return (comparison.length == 0)
+  }
