a9f90cb4253c7d5d64ca378a5819198f5660e7f8
Bradley Adams <bradley.wayne.adams@gmail.com>
2018-09-17 14:58:47 +1200

    disemvowel trolls answer

--- code.js
+++ code.js
@@ -1,52 +1,74 @@
 // One: Format a string of names like 'Bart, Lisa & Maggie'.
-function list(names){
+function list(names) {
   let answer = ''
-  
-  for (i = 0; i < names.length; i++){
+
+  for (i = 0; i < names.length; i++) {
     var handle = names[i];
     answer += handle.name +
-      (i < (names.length - 2) ? ', ' : 
+      (i < (names.length - 2) ? ', ' :
         (i === (names.length - 2) ? ' & ' : ''));
   }
   return answer
 }
 
 // Two: Valid Braces
-function validBraces(braces){
+function validBraces(braces) {
 
   let comparison = []
   let matching
   let open = ['(', '{', '[']
   let closed = [')', '}', ']']
-  
-  for (let i=0; i < braces.length; i++){
+
+  for (let i = 0; i < braces.length; i++) {
     let position = braces[i]
-    
-    if (closed.indexOf(position) > -1){
+
+    if (closed.indexOf(position) > -1) {
       matching = open[closed.indexOf(position)]
-      if (comparison.lengeth == 0 || (comparison.pop() != matching)){
-      return false
+      if (comparison.lengeth == 0 || (comparison.pop() != matching)) {
+        return false
       }
     } else {
-    comparison.push(position)}
-    
+      comparison.push(position)
     }
-    return (comparison.length == 0)
+
+  }
+  return (comparison.length == 0)
+}
+
+// Three: Printer Errors
+function printerError(s) {
+  s.split(' ')
+  let bl = ['n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  let count = 0
+  let slength = s.length
+  let bllength = bl.length
+  for (let i = 0; i < slength; i++) {
+    for (let j = 0; j < bllength; j++) {
+      if (s[i] === bl[j]) {
+        count++
+      }
+    }
+  }
+  return count + '/' + slength
+}
+
+
+// Four: Disemvowel Trolls
+function disemvowel(str) {
+  return str =  str.replace(/a|e|i|o|u/gi, "")
   }
 
-  // Three: Printer Errors
-  function printerError(s) {
-    s.split(' ')
-    let bl = ['n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    let count = 0
-    let slength = s.length
-    let bllength = bl.length
-    for (let i = 0; i < slength; i++) {
-      for (let j = 0; j < bllength; j++){
-        if ( s[i] === bl[j]){
-        count++
-        }
+function disemvowel(str) {
+  str.split(' ')
+  let vowels = ['a', 'e', 'i', 'o', 'u']
+  let slength = str.length
+  let vlength = vowels.length
+  console.log(str)
+  for (let i = 0; i < slength; i++){
+    for (let j = 0; j < vlength; j++){
+      if (str[i] == vowels[j]){
+        delete str[i]
       }
-      }
-      return count + '/' + slength
-    }
+  }
+  }
+  }
